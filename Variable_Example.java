94f5ba1482cdbae3b12b981cec9396cb2ccbc024
AnjaliBoddupally2 <159931433+AnjaliBoddupally2@users.noreply.github.com>
2024-02-19 17:03:00 +0530

    Add files via upload

---
+++ Variable_Example.java
@@ -0,0 +1,12 @@
+public class Variable_Example
+{ 
+public static void main(String[] args)
+{
+int number2=13;
+if(number2>=12){
+System.out.println("number1 and number2 values are not equal");
+}
+else
+System.out.println("number1 and number2 values are equal");
+}
+}
---
+++ Variable_Example1.java
@@ -0,0 +1,15 @@
+public class Variable_Example1
+{ 
+public static void main(String[] args)
+{
+int number1=12;
+int number2=13;
+int number3=11;
+int result=number1+number2+number3;
+int avg=result/3;
+int multiplication = number1*number2*number3;
+System.out.println("numer1+number2+number3:"+result);
+System.out.println("(numer1+number2+number3)/3 :"+ avg);
+System.out.println("(numer*number2*number3) :"+ multiplication);
+}
+}
